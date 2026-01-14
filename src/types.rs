39309a597182f91eb75793f8aad05294ca0a756c
Hiroaki Yutani <yutani@mierune.co.jp>
2026-01-14 21:35:23 +0900

    Remove unused

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,3 @@
-use std::sync::Arc;
-use std::sync::Mutex;
-
-use crate::reader::GpkgDataSource;
-
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(C)]
 pub enum ColumnType {
@@ -14,33 +9,7 @@ pub enum ColumnType {
 }
 
 #[derive(Clone, Debug)]
-#[repr(C)]
 pub struct ColumnSpec {
     pub name: String,
     pub column_type: ColumnType,
-}
-
-#[repr(C)]
-pub struct GpkgBindData {
-    pub sources: Vec<GpkgDataSource>,
-    pub column_specs: Vec<ColumnSpec>,
-}
-
-pub struct Cursor {
-    pub source_idx: usize,
-    pub offset: usize,
-}
-
-#[repr(C)]
-pub struct StReadMultiInitData {
-    pub cursor: Arc<Mutex<Cursor>>,
-}
-
-impl Cursor {
-    pub fn new() -> Self {
-        Self {
-            source_idx: 0,
-            offset: 0,
-        }
-    }
 }
