e9962deaed76e2ae65010ac35286172462f145fd
Hiroaki Yutani <yutani@mierune.co.jp>
2026-01-14 22:13:10 +0900

    Re-export more

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-mod ogc_sql;
 mod gpkg;
+mod ogc_sql;
 mod types;
 
 pub use gpkg::Gpkg;
 // Re-export types used in public fields to keep the public API stable.
+pub use rusqlite::types::Value;
 pub use wkb::reader::{Dimension, GeometryType};
