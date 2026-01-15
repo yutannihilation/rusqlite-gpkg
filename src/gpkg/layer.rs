96390c39973b1674092a71338dbb69dd0483223b
Hiroaki Yutani <yutani.ini@gmail.com>
2026-01-15 21:03:43 +0900

    Insert 4326 to gpkg_spatial_ref_sys

--- src/gpkg/layer.rs
+++ src/gpkg/layer.rs
@@ -334,20 +334,6 @@ mod tests {
         Ok(())
     }
 
-    // This is a bit horrible part. gpkg_spatial_ref_sys requires the WKT of the SRS, but we don't have a good source for this.
-    // Adding 4326 is easy, but what should I do to support other SRS?
-    fn ensure_srs_4326(gpkg: &Gpkg) -> Result<()> {
-        const EPSG4326_WKT: &str = r#"GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]]"#;
-
-        gpkg.connection().execute(
-            "INSERT INTO gpkg_spatial_ref_sys \
-            (srs_name, srs_id, organization, organization_coordsys_id, definition, description) \
-            VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-            rusqlite::params!["WGS 84", 4326, "EPSG", 4326, EPSG4326_WKT, "WGS 84"],
-        )?;
-        Ok(())
-    }
-
     #[test]
     fn reads_generated_layers_and_counts() -> Result<()> {
         let gpkg = Gpkg::open_read_only(generated_gpkg_path())?;
@@ -395,7 +381,6 @@ mod tests {
     #[test]
     fn creates_layer_metadata() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
         let columns = vec![
             ColumnSpec {
                 name: "name".to_string(),
@@ -437,7 +422,6 @@ mod tests {
     #[test]
     fn inserts_and_updates_by_primary_key() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
         let columns = vec![
             ColumnSpec {
                 name: "name".to_string(),
@@ -482,7 +466,6 @@ mod tests {
     #[test]
     fn roundtrips_all_geometry_types() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
 
         let line = LineString::from(vec![(0.0, 0.0), (1.5, 1.0), (2.0, 0.5)]);
         let line_b = LineString::from(vec![(-1.0, -1.0), (-2.0, -3.0)]);
@@ -565,7 +548,6 @@ mod tests {
     #[test]
     fn roundtrips_z_and_m_dimensions() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
 
         let point_z = Wkt::from_str("POINT Z (1 2 3)")
             .map_err(|err| crate::error::GpkgError::Message(err.to_string()))?;
@@ -602,7 +584,6 @@ mod tests {
     #[test]
     fn rtree_updates_on_insert_update_delete() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
         let columns: Vec<ColumnSpec> = Vec::new();
         let layer = gpkg.new_layer(
             "rtree_points",
@@ -653,7 +634,6 @@ mod tests {
     #[test]
     fn truncates_rows() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
         let columns = vec![ColumnSpec {
             name: "name".to_string(),
             column_type: ColumnType::Varchar,
@@ -687,7 +667,6 @@ mod tests {
     #[test]
     fn rejects_invalid_property_count() -> Result<()> {
         let gpkg = Gpkg::new_in_memory()?;
-        ensure_srs_4326(&gpkg)?;
         let columns = vec![
             ColumnSpec {
                 name: "name".to_string(),
--- src/ogc_sql.rs
+++ src/ogc_sql.rs
@@ -104,6 +104,7 @@ pub(crate) fn sql_insert_feature(layer_name: &str, columns: &str, values: &str) 
 
 pub(crate) fn initialize_gpkg(conn: &rusqlite::Connection) -> rusqlite::Result<()> {
     conn.execute_batch(SQL_GPKG_SPATIAL_REF_SYS)?;
+    ensure_srs_4326(conn)?;
     conn.execute_batch(SQL_GPKG_CONTENTS)?;
     conn.execute_batch(SQL_GPKG_GEOMETRY_COLUMNS)?;
     conn.execute_batch(SQL_GPKG_TILE_MATRIX_SET)?;
@@ -141,6 +142,20 @@ CREATE TABLE gpkg_spatial_ref_sys (
   description TEXT
 );
 ";
+
+// This is a bit horrible part. gpkg_spatial_ref_sys requires the WKT of the SRS, but we don't have a good source for this.
+// Adding 4326 is easy, but what should I do to support other SRS?
+fn ensure_srs_4326(conn: &rusqlite::Connection) -> rusqlite::Result<()> {
+    const EPSG4326_WKT: &str = r#"GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]]"#;
+
+    conn.execute(
+        "INSERT INTO gpkg_spatial_ref_sys \
+            (srs_name, srs_id, organization, organization_coordsys_id, definition, description) \
+            VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+        rusqlite::params!["WGS 84", 4326, "EPSG", 4326, EPSG4326_WKT, "WGS 84"],
+    )?;
+    Ok(())
+}
 
 // gpkg_tile_matrix: documents tile pyramid structure per zoom level (tile size,
 // matrix size, and pixel sizes) to support non-square tiles and varied intervals.
