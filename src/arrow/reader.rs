d7ba08664c83c8f7b9fe6d26fcdec631a4b676a3
Hiroaki Yutani <yutani.ini@gmail.com>
2026-04-05 15:55:33 +0900

    Use PROJJSON in reader

--- src/arrow/reader.rs
+++ src/arrow/reader.rs
@@ -56,7 +56,7 @@ impl<'a> ArrowGpkgReader<'a> {
         let schema_ref = Self::construct_arrow_schema(
             &layer.property_columns,
             &layer.geometry_column,
-            &layer.srs_id.to_string(),
+            layer.srs_id,
         );
 
         Self {
@@ -73,7 +73,7 @@ impl<'a> ArrowGpkgReader<'a> {
     fn construct_arrow_schema(
         property_columns: &[ColumnSpec],
         geometry_column: &str,
-        srs_id: &str,
+        srs_id: u32,
     ) -> SchemaRef {
         let mut fields: Vec<FieldRef> = property_columns
             .iter()
@@ -97,19 +97,14 @@ impl<'a> ArrowGpkgReader<'a> {
                     crate::ColumnType::Blob => {
                         arrow_schema::Field::new(&col.name, arrow_schema::DataType::Binary, true)
                     }
-                    crate::ColumnType::Geometry => {
-                        wkb_geometry_field(&col.name, srs_id.to_string())
-                    }
+                    crate::ColumnType::Geometry => wkb_geometry_field(&col.name, srs_id),
                 };
 
                 Arc::new(field)
             })
             .collect();
 
-        fields.push(Arc::new(wkb_geometry_field(
-            geometry_column,
-            srs_id.to_string(),
-        )));
+        fields.push(Arc::new(wkb_geometry_field(geometry_column, srs_id)));
 
         Arc::new(arrow_schema::Schema::new(fields))
     }
@@ -146,17 +141,16 @@ impl<'a> ArrowGpkgReader<'a> {
                         8 * self.batch_size,
                     ))
                 }
-                crate::ColumnType::Geometry => GpkgArrayBuilder::Geometry(wkb_geometry_builder(
-                    self.srs_id.to_string(),
-                    self.batch_size,
-                )),
+                crate::ColumnType::Geometry => {
+                    GpkgArrayBuilder::Geometry(wkb_geometry_builder(self.srs_id, self.batch_size))
+                }
             })
             .collect();
 
         GpkgRecordBatchBuilder {
             schema_ref: self.schema_ref.clone(),
             builders,
-            geo_builder: wkb_geometry_builder(self.srs_id.to_string(), self.batch_size),
+            geo_builder: wkb_geometry_builder(self.srs_id, self.batch_size),
         }
     }
 
@@ -378,16 +372,25 @@ fn rusqlite_value_type_name(value: &rusqlite::types::Value) -> &'static str {
     }
 }
 
-fn wkb_geometry_field(field_name: &str, srs_id: String) -> arrow_schema::Field {
-    let geoarrow_metadata =
-        geoarrow_schema::Metadata::new(geoarrow_schema::Crs::from_srid(srs_id.clone()), None);
+fn crs_from_srs_id(srs_id: u32) -> geoarrow_schema::Crs {
+    match epsg_utils::epsg_to_projjson(srs_id as i32) {
+        Ok(projjson_str) => {
+            let value: serde_json::Value =
+                serde_json::from_str(projjson_str).expect("PROJJSON from epsg-utils must be valid");
+            geoarrow_schema::Crs::from_projjson(value)
+        }
+        Err(_) => geoarrow_schema::Crs::from_srid(srs_id.to_string()),
+    }
+}
+
+fn wkb_geometry_field(field_name: &str, srs_id: u32) -> arrow_schema::Field {
+    let geoarrow_metadata = geoarrow_schema::Metadata::new(crs_from_srs_id(srs_id), None);
     geoarrow_schema::GeoArrowType::Wkb(geoarrow_schema::WkbType::new(geoarrow_metadata.into()))
         .to_field(field_name, true)
 }
 
-fn wkb_geometry_builder(srs_id: String, batch_size: usize) -> WkbBuilder<i32> {
-    let geoarrow_metadata =
-        geoarrow_schema::Metadata::new(geoarrow_schema::Crs::from_srid(srs_id.clone()), None);
+fn wkb_geometry_builder(srs_id: u32, batch_size: usize) -> WkbBuilder<i32> {
+    let geoarrow_metadata = geoarrow_schema::Metadata::new(crs_from_srs_id(srs_id), None);
     WkbBuilder::with_capacity(
         geoarrow_schema::WkbType::new(geoarrow_metadata.into()),
         geoarrow_array::capacity::WkbCapacity::new(21 * batch_size, batch_size),
